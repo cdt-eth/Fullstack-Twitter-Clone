245e55e61ac90217e2f017430250ce007628d695
christianturner <christianturner@hey.com>
2021-08-12 16:48:15 -0400

    set up httpLink for local storage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,30 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import "./App.css";
 import Users from "./components/Users";
+import Landing from "./components/Landing";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "apollo-link-context";
 
+const httpLink = new HttpLink({ uri: process.env.REACT_APP_URI });
+const authLink = setContext(async (req, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    ...headers,
+    headers: {
+      Authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const link = authLink.concat(httpLink as any);
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_URI,
+  link: link as any,
   cache: new InMemoryCache(),
 });
 
@@ -15,6 +35,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Users />
+          </Route>
+          <Route path="/landing">
+            <Landing />
           </Route>
         </Switch>
       </Router>
